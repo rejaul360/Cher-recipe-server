25dd180700adaf1fefe2c6b91fa151ed6fc847dd
rejaul360 <rejaraju2k22@gmail.com>
2023-05-04 12:24:50 +0600

    send recipe id

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.use(cors())
 
 const chefData = require('./data/chef.json')
 
+app.get('/chefData', (req, res) => {
+  res.send(chefData);
+})
+
+
+app.get('/chefData/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const items = chefData.find(chef => chef.id === id);
+  res.send(items);
+})
+
 app.get('/', (req, res) => {
   res.send('chef recipe hunter website')
 })
